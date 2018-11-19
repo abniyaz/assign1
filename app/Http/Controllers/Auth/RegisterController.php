b8111bbc0429177df08370ac3819caf6db754b53
niyaz <niyanote@gmail.com>
2018-11-19 11:49:59 +0800

    lease enter the commit message for your changes. Lines starting

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,8 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'mNumber'=>'max:255',
+            'utype'=>'max:255',
         ]);
     }
 
@@ -67,6 +69,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'mobile'=>$data['mNumber'],
+            'userType'=>$data['utype'],
         ]);
     }
 }
